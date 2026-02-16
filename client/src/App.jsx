3c03ae2a1b0320ee44bc54baec4e8101a922e6a5
felixjonus07 <felixjonus07@gmail.com>
2026-02-16 20:46:23 +0530

    Added Styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './navbar';
+import Courses from './courses';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Courses />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/courses.jsx
@@ -0,0 +1,15 @@
+function Courses(){
+    return(
+        <div>
+            <h1>Courses</h1>
+            <div className="course">
+                <img src="" alt="" />
+                <h2>Course Name</h2>
+                <p>Course Description</p>
+                <button>Enroll</button>
+            </div>
+        </div>
+    );
+}
+
+export default Courses; 
